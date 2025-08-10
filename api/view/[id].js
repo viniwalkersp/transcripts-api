b8b5ae72840f9739fdac69b65c58fe22a0d66f9f
Vini <137384282+viniwalkersp@users.noreply.github.com>
2025-08-09 23:40:24 -0300

    Delete api/view/[id].js

--- api/view/[id].js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { id } = req.query;
-
-  // Monta o link do blob original
-  const blobUrl = `https://ffmoxgz5bqepn5e.public.blob.vercel-storage.com/${id}.html`;
-
-  // Faz redirecionamento 302
-  res.redirect(blobUrl);
-}
