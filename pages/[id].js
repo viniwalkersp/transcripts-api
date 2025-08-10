c1a48d1c695fa78fe9cd95bf918444261ac75a2e
Vini <137384282+viniwalkersp@users.noreply.github.com>
2025-08-09 23:40:44 -0300

    Add files via upload

---
+++ pages/[id].js
@@ -0,0 +1,15 @@
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const blobUrl = `https://ffmoxgz5bqepn5e.public.blob.vercel-storage.com/${id}.html`;
+
+  return {
+    redirect: {
+      destination: blobUrl,
+      permanent: false,
+    },
+  };
+}
+
+export default function RedirectPage() {
+  return null;
+}
